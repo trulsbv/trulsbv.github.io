09db061e7a521706a31256f4b25763375b0fe369
Truls Vidnes <114477568+trulsbv@users.noreply.github.com>
2025-08-07 10:33:11 +0100

    Link to button instead of Hero

--- src/pages/components/ComponentsPage.tsx
+++ src/pages/components/ComponentsPage.tsx
@@ -6,13 +6,13 @@ export const ComponentsPage = () => (
     <h1>Components</h1>
     <ul>
       <li>
-        <Link to={ROUTES.COMPONENT_DETAIL.replace(":componentName", "hero")}>
-          Hero
+        <Link to={ROUTES.COMPONENT_DETAIL.replace(":componentName", "modal")}>
+          Modal
         </Link>
       </li>
       <li>
-        <Link to={ROUTES.COMPONENT_DETAIL.replace(":componentName", "modal")}>
-          Modal
+        <Link to={ROUTES.COMPONENT_DETAIL.replace(":componentName", "button")}>
+          Button
         </Link>
       </li>
     </ul>
